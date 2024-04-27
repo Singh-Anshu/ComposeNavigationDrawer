6fb20122da474e59f62c852d0ec1bbf0a2b82d34
Anshu Singh <anshusingh7738@gmail.com>
2024-04-28 02:03:10 +0530

    making zomato DashBoard.

--- app/src/main/java/com/example/navigationdrawer/MainActivity.kt
+++ app/src/main/java/com/example/navigationdrawer/MainActivity.kt
@@ -1,20 +1,34 @@
 package com.example.navigationdrawer
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.Info
@@ -34,20 +48,37 @@ import androidx.compose.material3.NavigationDrawerItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Dimension
+import com.example.navigationdrawer.ui.theme.MyGrey
+import com.example.navigationdrawer.ui.theme.MyRed
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
@@ -74,19 +105,240 @@ class MainActivity : ComponentActivity() {
                 ),
             )
 
-
             Surface(
                 modifier = Modifier.fillMaxSize(),
                 color = MaterialTheme.colorScheme.background
 
             ) {
+//                NavigationDrawerDemo(items)
+                DashBoard()
 
-
-                NavigationDrawerDemo(items)
             }
 
         }
     }
+}
+
+@Composable
+fun DashBoard() {
+
+    Column(
+        modifier = Modifier
+            .fillMaxHeight()
+            .fillMaxWidth()
+            .background(MyGrey),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        ConstraintLayout {
+            val (redBox) = createRefs()
+
+            Box(
+                modifier = Modifier.fillMaxWidth()
+                    .height(70.dp)
+                    .constrainAs(redBox) {
+                        top.linkTo(parent.top)
+                        start.linkTo(parent.start)
+                    }.background(color = MyRed)
+            )
+
+            Row(
+                modifier = Modifier
+                    .padding(top = 10.dp, start = 10.dp, end = 24.dp)
+                    .fillMaxWidth()
+            ) {
+
+                Column(
+                    modifier = Modifier
+                        .height(60.dp)
+                        .padding(start = 8.dp)
+                        .weight(0.7f),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.Start
+                ) {
+
+                    Text(
+                        text = "Zomato",
+                        fontSize = 28.sp,
+                        fontWeight = FontWeight.Bold,
+                        fontStyle = FontStyle.Italic,
+                        color = Color.White
+                    )
+                }
+
+                Image(painter = painterResource(R.drawable.frame_8), contentDescription = "person",
+                    modifier = Modifier.height(50.dp).clickable { })
+
+            }
+        }
+
+        /*var searchText by rememberSaveable() { mutableStateOf("") }-
+        val isFocused = remember { mutableStateOf(false) }
+
+        TextField(
+            value = searchText, onValueChange = { searchText = it },
+            label = { if (isFocused.value) null else Text(text = "Search restaurants...") },
+            trailingIcon = {
+                Image(
+                    imageVector = Icons.Filled.Search, contentDescription = "Search",
+                    modifier = Modifier.size(38.dp).padding(end = 8.dp)
+                )
+            },
+
+            textStyle = TextStyle(
+                color = if (searchText.isEmpty()) Color.Gray else Color.Black,
+                fontSize = if (searchText.isEmpty()) 16.sp else 18.sp,
+
+            ),
+            shape = RoundedCornerShape(50.dp),
+            colors = TextFieldDefaults.colors(
+                unfocusedContainerColor = Color.White,
+                focusedContainerColor = Color.White,
+                focusedPlaceholderColor = Color.White,
+                cursorColor = Color(0xFFCB202D),
+            ),
+            singleLine = true,
+            maxLines = 1,
+
+            keyboardOptions = KeyboardOptions.Default.copy(autoCorrect = true),
+            modifier = Modifier.fillMaxWidth().height(70.dp)
+                .padding(top = 24.dp, start = 24.dp, end = 24.dp)
+                .shadow(3.dp, shape = RoundedCornerShape(50.dp))
+                .background(color = Color.White, CircleShape)
+                .onFocusChanged { focusState ->
+                    isFocused.value = focusState.isFocused
+                }
+        )*/
+
+        Spacer(modifier = Modifier.height(24.dp))
+        MySearchableComponent()
+
+        ConstraintLayout(
+            modifier = Modifier.fillMaxWidth().height(150.dp)
+                .padding(top = 24.dp, start = 24.dp, end = 24.dp)
+                .shadow(5.dp, shape = RoundedCornerShape(20.dp))
+                .background(color = MyRed)
+
+        ) {
+            val (bannerImage, flatText, freeText, couponText) = createRefs()
+
+            Image(
+                modifier = Modifier.padding(start = 50.dp).constrainAs(bannerImage) {
+                    top.linkTo(parent.top)
+                    end.linkTo(parent.end)
+                    bottom.linkTo(parent.bottom)
+                    height = Dimension.value(130.dp)
+                },
+                contentDescription = "Banner Image",
+                painter = painterResource(R.drawable.frame_8)
+            )
+            Text(text = "FLAT 50% OFF",
+                fontSize = 28.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.White,
+                modifier = Modifier.padding(top = 32.dp).constrainAs(flatText) {
+                    top.linkTo(parent.top)
+                    end.linkTo(bannerImage.start)
+                    start.linkTo(parent.start)
+                })
+
+            Text(text = "Free Deliver + 10% Cashback",
+                fontSize = 12.sp,
+                color = Color.White,
+                modifier = Modifier.constrainAs(freeText) {
+                    top.linkTo(flatText.bottom)
+                    end.linkTo(flatText.end)
+                    start.linkTo(flatText.start)
+                })
+
+            Text(text = "Coupon F00D50",
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.White,
+                modifier = Modifier.padding(top = 8.dp).constrainAs(couponText) {
+                    top.linkTo(freeText.bottom)
+                    end.linkTo(freeText.end)
+                    start.linkTo(freeText.start)
+                })
+
+        }
+    }
+}
+
+@Composable
+fun SearchBar(onSearch: (String) -> Unit) {
+    var searchText by remember { mutableStateOf("") }
+    val isTextEmpty = searchText.isEmpty()
+
+    TextField(
+        value = searchText,
+        onValueChange = {
+            searchText = it
+            onSearch(it)
+        },
+        singleLine = true,
+        maxLines = 1,
+        placeholder = { Text("Search...") },
+        trailingIcon = {
+            if (!isTextEmpty) {
+                Icon(
+                    imageVector = Icons.Filled.Close,
+                    contentDescription = "Clear Text",
+                    modifier = Modifier
+                        .size(38.dp)
+                        .padding(end = 8.dp)
+                        .clickable {
+                            searchText = ""
+                            onSearch("")
+                        }
+                )
+            } else {
+                Image(
+                    imageVector = Icons.Filled.Search, contentDescription = "Search Icon",
+                    modifier = Modifier.size(38.dp).padding(end = 8.dp)
+                )
+            }
+        },
+        textStyle = TextStyle(
+            color = if (!isTextEmpty) Color.Black else Color.Gray,
+            fontSize = 18.sp,
+        ),
+
+        colors = TextFieldDefaults.colors(
+            unfocusedContainerColor = Color.White,
+            focusedContainerColor = Color.White,
+            focusedPlaceholderColor = Color.White,
+            cursorColor = Color(0xFFCB202D),
+            focusedIndicatorColor = Color.Transparent, // Hide the underline when focused
+            unfocusedIndicatorColor = Color.Transparent // Hide the underline always
+        ),
+        shape = RoundedCornerShape(50.dp),
+        modifier = Modifier.fillMaxWidth().height(56.dp)
+            .padding(start = 24.dp, end = 24.dp)
+            .shadow(3.dp, shape = RoundedCornerShape(50.dp))
+            .background(color = Color.White, CircleShape),
+
+        keyboardOptions = KeyboardOptions.Default.copy(
+            imeAction = ImeAction.Search,
+            autoCorrect = true
+        ),
+        keyboardActions = KeyboardActions(onSearch = {
+            onSearch(searchText) // Handle the search action (e.g., querying a database or filtering a list)
+            defaultKeyboardAction(ImeAction.Search)
+        })
+    )
+}
+
+@Composable
+fun MySearchableComponent() {
+    SearchBar { searchText ->
+        // Here you would typically handle the search query, such as filtering a list
+        Log.d(
+            "MySearchableComponent",
+            "Searching for: $searchText"
+        ) // Placeholder for actual search logic
+    }
+
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -205,7 +457,7 @@ fun LearnCostraintLayout() {
                 top.linkTo(redButton.bottom)
                 start.linkTo(parent.start)
 
-               // end.linkTo(blueButton.start)
+                // end.linkTo(blueButton.start)
 
             }) {
             Text(text = "Green Button")
@@ -214,14 +466,14 @@ fun LearnCostraintLayout() {
         Button(onClick = {}, colors = ButtonDefaults.buttonColors(Color.Blue),
             modifier = Modifier.constrainAs(blueButton) {
                 top.linkTo(redButton.bottom)
-               // start.linkTo(greenButton.end)
+                // start.linkTo(greenButton.end)
                 end.linkTo(parent.end)
 
             }) {
             Text(text = "Blue Button")
         }
 
-        Button(onClick = {}, colors = ButtonDefaults.buttonColors(Color.Black),
+        Button(onClick = {}, colors = ButtonDefaults.buttonColors(MyRed),
             modifier = Modifier.constrainAs(blackButton) {
 
                 top.linkTo(blueButton.bottom)
--- app/src/main/java/com/example/navigationdrawer/ui/theme/Color.kt
+++ app/src/main/java/com/example/navigationdrawer/ui/theme/Color.kt
@@ -1,5 +1,6 @@
 package com.example.navigationdrawer.ui.theme
 
+import android.provider.CalendarContract.Colors
 import androidx.compose.ui.graphics.Color
 
 val Purple80 = Color(0xFFD0BCFF)
@@ -9,3 +10,6 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val MyRed = Color(0xFFCB202D)
+val MyGrey = Color(0xFFeeeefb)
